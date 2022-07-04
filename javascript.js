74f98c285106bc3d6a8740b7a560a5259f5afe1b
Jando <alexandroguterez@gmail.com>
2022-07-03 18:32:47 -0700

    setup template for overall page layout

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 async function getWeather(city) {
     const rawData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3cd7c9b2e16fbaba921ae2be63178bf9`);
     const data = await rawData.json();
-    console.log(data);
+    return data;
 }
-getWeather('Oakland');
+let name = getWeather('Oakland').then(data => {
+    console.log(data)
+})
